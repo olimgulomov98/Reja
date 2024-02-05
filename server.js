c5e7cfd4d84c3b9eec4864272b49e8bef9b88f6e
Olim <olimgulomov@gmail.com>
2024-02-05 04:42:01 +0400

    feat: express framework

--- server.js
+++ server.js
@@ -15,11 +15,13 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4: Routing code
-app.get("/hello", function (req, res) {
-  res.end(`<h1 style="background: red">Hello World by Saad</h1>`);
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  res.json({ test: "success" });
 });
-app.get("/gift", function (req, res) {
-  res.end(`<h1>Siz sovgalar bolimidasiz</h1>`);
+
+app.get("/", function (req, res) {
+  res.render("harid");
 });
 
 const server = http.createServer(app);
