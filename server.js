c34f3c33198e59359ca5e78ad91812209d08b205
Olim <olimgulomov@gmail.com>
2024-02-15 08:59:01 +0400

    fix: refactoring

--- server.js
+++ server.js
@@ -35,11 +35,13 @@ app.get("/author", (req, res) => {
 });
 
 app.get("/", function (req, res) {
-  res.render("harid");
+  res.render("reja");
 });
 
 const server = http.createServer(app); // shu app joylashlash joy singlethread deyiladi, xamma zaproslar shu yerga keladi
 let PORT = 3000;
 server.listen(PORT, function () {
-  console.log(`The server is running succesfully on port: ${PORT}`);
+  console.log(
+    `The server is running succesfully on port: ${PORT}, http://localhost:${PORT}`
+  );
 });
